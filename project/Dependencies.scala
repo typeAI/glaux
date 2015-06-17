b4ea9b8e3734fc65c1aeef847fc61cb637e6c824
Kailuo Wang <kailuo.wang@gmail.com>
2015-06-16 21:00:48 -0400

    tested GPU, worked but slow with small matrix

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ import sbt._
 object Dependencies {
 
   object Versions {
-    val nd4j = "0.0.3.5.5.4"
+    val nd4j = "0.0.3.5.5.5"
   }
 
   val resolvers = Seq(
@@ -23,7 +23,8 @@ object Dependencies {
   )
 
   val math = Seq (
-    "org.nd4j" % "nd4j-jblas" % Versions.nd4j,
+    "org.nd4j" % "nd4j-jblas" % Versions.nd4j,  //switch between the following line to use GPU
+//  "org.nd4j" % "nd4j-jcublas-7.0" % Versions.nd4j,
     "org.nd4j" % "nd4j-api" % Versions.nd4j
   )
 
